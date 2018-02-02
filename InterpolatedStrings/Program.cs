03c9e05a7ca4919790734bc125e2d5dbd99f26aa
AustinKurtis <austin.jay.kurtis@gmail.com>
2018-02-02 13:18:14 -0600

    Include different data types

--- InterpolatedStrings/Program.cs
+++ InterpolatedStrings/Program.cs
@@ -4,11 +4,31 @@ namespace InterpolatedStrings
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            var name = "Austin";
-            Console.WriteLine($"Hello, {name} it's a pleasure to meet you.");
+        //static void Main(string[] args)
+        //{
+        //    var name = "Austin";
+        //    Console.WriteLine($"Hello, {name} it's a pleasure to meet you.");
+        //}
+    }
+    public class Vegetable
+    {
+        public Vegetable(string name) => Name = name;
 
+        public string Name { get; }
+
+        public override string ToString() => Name;
+    }
+    public class Example
+    {
+        public enum Unit { item, pound, ounce, dozen };
+
+        public static void Main()
+        {
+            var item = new Vegetable("eggplant");
+            var date = DateTime.Now;
+            var price = 1.99m;
+            var unit = Unit.item;
+            Console.WriteLine($"On {date}, the price of {item} was {price} per {unit}.");
         }
     }
 }
